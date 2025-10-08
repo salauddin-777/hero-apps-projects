b06d9f63881ec7e69c2be1b657f7af3d95bc4f30
salauddin-777 <mdsalauddinmina777@gmail.com>
2025-10-08 21:14:26 +0600

    Sorted items functionality added with High to Low and Low to High By downloads

--- src/Pages/Installation.jsx
+++ src/Pages/Installation.jsx
@@ -3,10 +3,22 @@ import InstallSingleCard from "../Components/InstallSingleCard";
 
 const Installation = () => {
   const [installList, setInstallList] = useState([]);
+  const [sortOrder, setSortOrder] = useState('none')
   useEffect(() => {
     const savedList = JSON.parse(localStorage.getItem("installedList"));
     if (savedList) setInstallList(savedList);
   }, []);
+
+  const sortedItem = (()=>{
+    if(sortOrder === 'download-asc'){
+        return [...installList].sort((a,b)=> b.downloads - a.downloads)
+    }else if(sortOrder === 'download-dsc'){
+        return [...installList].sort((a,b)=> a.downloads - b.downloads)
+    }else{
+        return installList;
+    }
+  }) ()
+
   return (
     <div className="container mx-auto">
       <div className="p-15 text-center">
@@ -18,24 +30,20 @@ const Installation = () => {
       <div className="flex justify-between items-center my-5">
         <h2 className="text-2xl font-bold ">({installList.length}) Apps Found</h2>
         {/* Drop Down */}
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn m-1">
-            Sort By Size
-          </div>
-          <ul
-            tabIndex={0}
-            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
-          >
-            <li>
-              <a>Sort By Download</a>
-            </li>
-          </ul>
-        </div>
+       <div>
+        <label className="form-control w-full mx-w-xs">
+        <select className="select select-bordered" value={sortOrder} onChange={e=>setSortOrder(e.target.value)} >
+            <option value="none">Sort By Download</option>
+            <option value="download-asc">High -&gt;Low</option>
+            <option value="download-dsc">Low -&gt; High</option>
+        </select>
+       </label>
+       </div>
       </div>
       {/* Card */}
       <div className="my-10">
         {
-            installList.map(app=> <InstallSingleCard key={app.id} app={app}></InstallSingleCard>)
+            sortedItem.map(app=> <InstallSingleCard key={app.id} app={app}></InstallSingleCard>)
         }
       </div>
     </div>
